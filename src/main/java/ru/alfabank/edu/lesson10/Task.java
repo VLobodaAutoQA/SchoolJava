05b2f656c12bbcaac10ad7ac76638fd6f9b60505
Лобода Владислав <vlad.loboda@example.com>
2026-07-03 17:13:10 +0300

    Полностью заполнил класс Task

--- src/main/java/ru/alfabank/edu/lesson10/Task.java
+++ src/main/java/ru/alfabank/edu/lesson10/Task.java
@@ -1,4 +1,33 @@
 package ru.alfabank.edu.lesson10;
 
 public class Task {
+    private String nameTask;
+    private boolean isDone;
+
+    public Task (String nameTask) {
+        this.nameTask = nameTask;
+    }
+    // пометить задачу как выполненную
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    // получить статус - выполнена задача или нет
+    public boolean getStatusTask() {
+        return isDone;
+    }
+
+    // получить название задачи
+    public String getNameTask() {
+        return nameTask;
+    }
+
+    // вывести информацию о задаче
+    public void printInfoTask() {
+        if (isDone) {
+            System.out.println("[x] " + nameTask);
+        } else {
+            System.out.println("[ ] " + nameTask);
+        }
+    }
 }
